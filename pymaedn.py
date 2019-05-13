9ff759efb9185a202703051c78228ec5ba728c8f
Kantospaghetti <50325631+Kantospaghetti@users.noreply.github.com>
2019-05-13 14:25:54 +0200

    Add files via upload

--- pymaedn.py
+++ pymaedn.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from random import randint
 
 PLAYERS = [
     "blue",
@@ -6,11 +7,37 @@ PLAYERS = [
     "green",
     "yellow"
 ]
+FIELD_SIZE = 4
+FIELD_COLOR = "black"
+FIELD_POSITIONS = [
+    (10, 4),
+    (9, 4),
+    (8, 4),
+    (7, 4),
+    (6, 4),
+    (6, 3),
+    (6, 2),
+    (6, 1),
+    (6, 0),
+    (5, 0),
+    (4, 0),
+    (4, 1),
+    (4, 2),
+    (4, 3),
+    (4, 4),
+    (3, 4),
+    (2, 4),
+    (1, 4),
+    (0, 4), (0, 5), (0, 6),
+    (1, 6), (2, 6), (3, 6)
+    
+] 
 
 class Field:
     """Graphical represantation of a field. """
-    def __init__(self, position, master):
-        self.label = Label(master=master)
+    def __init__(self, master, row, column):
+        self.label = Label(master=master, bg=FIELD_COLOR, width=FIELD_SIZE, height=FIELD_SIZE//2)
+        self.label.grid(row=row, column=column)
 
     def enter_player(self, player):
         """Change field color to player color."""
@@ -24,19 +51,27 @@ class Figure:
 
 class Player:
     """Virtual representation of a player."""
-    def __init__(self, number, color):
-        self.number = number
+    def __init__(self, color):
         self.color = color
 
 class Gamemanager:
     """Global core gameplay mechanics."""
     def __init__(self):
+        self.master = None
         self.players = []
+        self.board = []
 
     def create_board(self, master):
-        for number, color in enumerate(PLAYERS):
-            player = Player(number, color)
+        for color in PLAYERS:
+            # create player
+            player = Player(color)
             self.players.append(player)
+        # create board quater
+        for position in FIELD_POSITIONS:
+            (row, column) = position
+            field = Field(master, row, column)
+            self.board.append(field)
+            
             
 class Application:
     """Manage the Tkinter root window."""
