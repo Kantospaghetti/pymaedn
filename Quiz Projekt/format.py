de7390cfa14075333fd8390d8ade225a3c73acd8
Kantospaghetti <50325631+Kantospaghetti@users.noreply.github.com>
2019-06-03 07:28:27 +0200

    Add files via upload

--- Quiz Projekt/format.py
+++ Quiz Projekt/format.py
@@ -1,45 +1,97 @@
 from tkinter import *
-data = open("questions.txt", "r")
-output = open("questions.json", "w")
-questions = []
-question = {}
+from random import randint
+import re
 
-for line in data:
-    if line[0] == "\n":
-        questions.append(question)
-        question = {}
-    else:
-        key, value = line.split(":", 1)
-        question[key] = value.strip()
+ATTEMPTS = 3
+LEVEL_POINTS = {
+    "baby": 1,
+    "easy": 2,
+    "normal": 4,
+    "hard": 8,
+    "extreme": 10 
+}
 
-class Player:
-    def __init__(self):
-        self.points = 0
+class Question:
+    def __init__(self, Question="", Answer="", Level="normal", Category="Allgemein", Regexp=None, Tip=None):
+        self.value = Question
+        self.answer = Answer
+        self.level = Level
+        self.catergory = Category
+        self.regexp = Regexp
+        self.tip = Tip
 
-    def setPunkte():
-        if getAnswer(): points += 1
-        if getAnswer() == false: points -= 2
-    def getPunkte():
-        pass
-class Application:
+    def check_answer(self, answer):
+        if self.regexp:
+            return re.search(self.regexp.upper(), answer.upper())
+        else:
+            return answer.upper() == self.answer.upper()
+
+class Quiz:
     def __init__(self, root):
+        self.root = root
+        # tkinter
         root.title("Quiz")
-        root.geometry("600x600")
-        entryAnswer = Entry(master=root, bg="lightgrey")
-        entryAnswer.place(x=150, y=450, width=300, height=27)
+        self.main = Frame(master=root, padx=10)
+        self.main.pack(expand=True)
+        label_question_info = Label(master=self.main, text="Frage:")
+        label_question_info.pack()
+        self.label_question = Label(master=self.main, wraplength=300, text="loading...")
+        self.label_question.pack()
+        label_answer = Label(master=self.main, text="Ihre Antwort:")
+        label_answer.pack()
+        self.entry_answer = Entry(master=self.main, bg="lightgrey", width=50)
+        self.entry_answer.pack(fill=X, pady=5)
+        self.label_right_answer_info = Label(master=self.main, text="Richtige Antwort:")
+        self.label_right_answer = Label(master=self.main, text="loading...")
+        self.button_answer = Button(master=root, text="Antworten", bg="#4286f4", disabledforeground="black", width=50, height=2, \
+                                    command=lambda: self.validate_answer(self.entry_answer.get()))
+        self.button_answer.pack(pady=10, padx=20)
+        # gameplay
+        self.questions = []
+        self.question = None
+        self.points = 0
+        # start game
+        self.load_questions()
+        self.ask_question()
+
+    def load_questions(self):
+        data = open("questions.txt", "r", encoding='utf8')
+        question = {}
+        for line in data:
+            if line[0] == "\n":
+                self.questions.append(Question(**question))
+                question = {}
+            else:
+                key, value = line.split(":", 1)
+                question[key] = value.strip()
+    
+    def ask_question(self):
+        self.question = self.questions[randint(0, len(self.questions)-1)]
+        self.questions.remove(self.question)
+        self.label_question.config(text=self.question.value)
+        self.button_answer.config(state=NORMAL, bg="#4286f4")
+        self.label_right_answer_info.pack_forget()
+        self.label_right_answer.pack_forget()
+        self.entry_answer.delete(0, 'end')
+        self.entry_answer.focus_set()
+
+    def validate_answer(self, answer):
+        if self.question.check_answer(answer):
+            # correct answer
+            self.points += LEVEL_POINTS[self.question.level]
+            self.button_answer.config(state=DISABLED, bg="green")
+        else:
+            # wrong anser
+            self.points -= 1
+            self.button_answer.config(state=DISABLED, bg="red")
+            self.label_right_answer_info.pack()
+            self.label_right_answer.config(text=self.question.answer)
+            self.label_right_answer.pack()
+        self.root.after(2000, self.ask_question)
         
         
-
-
-
-    #def getAnswer():
-        #Answer = (entryAnswer.get())
-
-
 root = Tk()
-Application(root)
+Quiz(root)
+
 root.mainloop()
-
-
-
 
